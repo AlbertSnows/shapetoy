ea6fa62cb0a6035544117a9bb6fdbb1422ced558
Albert Snow <ajsnow2012@gmail.com>
2023-12-25 22:54:12 -0600

    minor stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ import { when } from "./helpers/utility"
 import * from "./helpers/logic"
 
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d")
+const context = canvas.getContext("2d");
 const when_canvas_exists = when(() => canvas.getContext !== null && canvas.getContext !== undefined);
 document.getElementById('generateCircle')
 	.addEventListener('click', () => when_canvas_exists(() => generateCircle(canvas)));
---
+++ docs/troubleshooting.md
@@ -0,0 +1,6 @@
+
+# Troubleshooting
+
+## JS doesn't show up for scripts labeled module
+
+has something to do with CQRS thing maybe? I gave up, don't use module.
