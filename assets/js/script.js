6b0592f9a29a88989c2ca666e5e8a95159268b43
Albert Snow <ajsnow2012@gmail.com>
2023-12-25 21:47:58 -0600

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-let canvas = document.getElementById('hello-world-canvas')
-let context = canvas.getContext('2d')
-let when_canvas_exists = when(() => canvas.getContext !== null && canvas.getContext !== undefined);
-document.getElementById('generateCircle').addEventListener('click', () => when_canvas_exists(generateCircle));
-document.getElementById('generateRectangle').addEventListener('click', when_canvas_exists(generateRectangle));
+import { when } from "./utility";
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d")
+const when_canvas_exists = when(() => canvas.getContext !== null && canvas.getContext !== undefined);
+document.getElementById('generateCircle')
+	.addEventListener('click', () => when_canvas_exists(generateCircle));
+document.getElementById('generateRectangle')
+	.addEventListener('click', when_canvas_exists(generateRectangle));
 
